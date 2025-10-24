29e97c02428f427a6d7b1802c79399d5cf5229c8
SentientExplosive <tnt.beep@gmail.com>
2025-10-24 18:42:38 -0400

    Realized I'm very bad at knowing what units I'm using

--- Code/ros_ws/src/nav_pkg/nav_pkg/nav_node.py
+++ Code/ros_ws/src/nav_pkg/nav_pkg/nav_node.py
@@ -113,8 +113,8 @@ class NaviConverter(Node):
         for waypoint in self.waypoints:
             self.instructions.append(f"t{waypoint[0]}")
             self.instructions.append(f"d{waypoint[1]}")
-            x = waypoint[1] * math.cos(waypoint[0])
-            y = waypoint[1] * math.sin(waypoint[0])
+            x = waypoint[1] * math.cos((waypoint[0])*math.pi/180)
+            y = waypoint[1] * math.sin(waypoint[0]*math.pi/180)
             self.get_logger().info(f"y: {y}")
             self.get_logger().info(f"x: {x}")
             vector_vals.append((x,y))
