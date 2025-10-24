e5c0ad6f6fb7d511a1b84c3073b937f11c073a80
SentientExplosive <tnt.beep@gmail.com>
2025-10-24 18:44:56 -0400

    Hopefully the last calculation fix

--- Code/ros_ws/src/nav_pkg/nav_pkg/nav_node.py
+++ Code/ros_ws/src/nav_pkg/nav_pkg/nav_node.py
@@ -127,7 +127,7 @@ class NaviConverter(Node):
             total_y += vec[1]
         self.get_logger().info(f"total_y: {total_y}")
         self.get_logger().info(f"total_x: {total_x}")
-        angle = math.atan2(total_y, total_x) * (180 / math.pi)
+        angle = math.atan2(-total_y, -total_x) * (180 / math.pi)
         if (angle < 0):
             angle += 360
         dist = math.sqrt((total_x)**2 + (total_y)**2)
